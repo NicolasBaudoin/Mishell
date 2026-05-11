147ca93781566a51b638035cf020ceaa9aff900a
nbaudoin <baudoin.nicolasg@gmail.com>
2026-05-11 20:39:34 +0200

    ok c pas mal, a voir pour retirer els quotes des tokens et l gestion dm erreur

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,19 +6,63 @@
 /*   By: nbaudoin <nbaudoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 16:22:23 by nbaudoin          #+#    #+#             */
-/*   Updated: 2026/05/11 19:44:39 by nbaudoin         ###   ########.fr       */
+/*   Updated: 2026/05/11 20:32:59 by nbaudoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
 
+void	read_word(t_token **tokens, t_lexer *lexer)
+{
+	int	start;
+	char	c;
+
+	start = lexer->i;
+	while (lexer->input[lexer->i])
+	{
+		c = lexer->input[lexer->i];
+		if (lexer->state == DEFAULT)
+		{
+			if (is_space(c) || is_operator(c))
+				break ;
+
+			if (c == '\'')
+			{
+				lexer->state = SINGLE_QUOTE;
+				lexer->i++;
+				continue ;
+			}
+
+			if (c == '"')
+			{
+				lexer->state = DOUBLE_QUOTE;
+				lexer->i++;
+				continue ;
+			}
+		}
+		else if (lexer->state == SINGLE_QUOTE && c == '\'')
+		{
+				lexer->state = DEFAULT;
+				lexer->i++;
+				continue ;
+		}
+		else if (lexer->state == DOUBLE_QUOTE && c == '"')
+		{
+				lexer->state = DEFAULT;
+				lexer->i++;
+				continue ;
+		}
+		lexer->i++;
+	}
+	add_back_token(tokens, new_token(ft_strndup(lexer->input, start, lexer->i), WORD));
+}
+
 t_token	*lexer(char *input)
 {
 	t_token	*tokens;
 	t_lexer	lexer;
-	int	start;
-	int	end;
+
 
 	tokens = NULL;
 	lexer.state = DEFAULT;
@@ -28,51 +72,13 @@ t_token	*lexer(char *input)
 	{
 		while (is_space(lexer.input[lexer.i]))
 			lexer.i++;
+		if (!lexer.input[lexer.i])
+			break ;
+
 		if (is_operator(lexer.input[lexer.i]))
 			create_operator_token(&lexer, &tokens);
-		if (is_word(lexer.input[lexer.i]) && lexer.input[lexer.i])
-		{
-			start = lexer.i;
-			while (!is_space(lexer.input[lexer.i]) && !is_operator(lexer.input[lexer.i]) && lexer.input[lexer.i])
-				lexer.i++;
-			end = lexer.i;
-			add_back_token(&tokens, new_token(ft_strndup(input, start, end), WORD));
-		}
+		else
+			read_word(&tokens, &lexer);
 	}
 	return (tokens);
 }
-
-
-
-// t_token	*lexer(char *input)
-// {
-// 	t_token	*tokens;
-// 	t_lexer	lexer;
-// 	int		start;
-// 	int		end;
-
-// 	tokens = NULL;
-// 	lexer.state = DEFAULT;
-// 	lexer.input = input;
-// 	lexer.i = 0;
-// 	while (lexer.input[lexer.i])
-// 	{
-// 		while (is_space(lexer.input[lexer.i]) && lexer.state != DOUBLE_QUOTE)
-// 			lexer.i++;
-// 		start = lexer.i; //sauvegarde start
-// 		if (is_operator(lexer.input[lexer.i]))
-// 			create_operator_token(lexer.input, start, 1, &tokens);	// créer operateur token
-// 		else
-// 			add_back_token(&tokens, new_token(ft_strndup(input, start, end), WORD));
-// 			// read word
-
-
-// 		// debut d'un element, si on rencontre un operateur
-// 		// separateur ou quote et qu'on est pas en mode double quote
-// 		// alors on change de token
-// 		// donc add_back(new_token)
-// 		// sinon :
-// 		lexer.i++;
-// 	}
-// 	return (tokens);
-// }
--- src/utils/detect_char_type.c
+++ src/utils/detect_char_type.c
@@ -6,7 +6,7 @@
 /*   By: nbaudoin <nbaudoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 17:34:26 by nbaudoin          #+#    #+#             */
-/*   Updated: 2026/05/11 19:18:13 by nbaudoin         ###   ########.fr       */
+/*   Updated: 2026/05/11 20:09:30 by nbaudoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,7 @@ int	is_space(char c)
 
 int	is_quote(char c)
 {
-	int	ret;
-
-	ret = (c == '"' || c == '\'');
-	return (ret);
+	return (c == '"' || c == '\'');
 }
 
 int	is_append(char *input, int start)
