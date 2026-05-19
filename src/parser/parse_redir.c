3edd3cfb6ebd355f1d3e27e23eccb8d7da16728f
nbaudoin <baudoin.nicolasg@gmail.com>
2026-05-19 23:56:05 +0200

    parse_redir done

--- src/parser/parse_redir.c
+++ src/parser/parse_redir.c
@@ -6,7 +6,7 @@
 /*   By: nbaudoin <nbaudoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 23:25:41 by nbaudoin          #+#    #+#             */
-/*   Updated: 2026/05/19 23:46:00 by nbaudoin         ###   ########.fr       */
+/*   Updated: 2026/05/19 23:54:26 by nbaudoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,17 @@ void	add_back_redir(t_redir **lst, t_redir *node_to_add)
 
 int	parse_redirs(t_token **token, t_cmd *cmd)
 {
+	t_redir *redir;
 
+	redir = new_redir();
+	if (!redir)
+		return (1);
+	redir->type = (*token)->type;
+	*token = (*token)->next;
+	redir->file = ft_strdup((*token)->value);
+	if (!redir->file)
+		return (1);
+	*token = (*token)->next;
+	add_back_redir(&cmd->redir, redir);
+	return (0);
 }
